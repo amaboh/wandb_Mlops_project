89b3e40fa76fdf4fcd28ba77a2713fc19eb07174
Ama <85511496+amaboh@users.noreply.github.com>
2023-08-20 23:38:19 +0100

    Update report.py

--- report.py
+++ report.py
@@ -1,4 +1,5 @@
 import re, os
+import wandb
 comment = os.getenv('PR_COMMENT', '')
 match = re.search('/wandb[\s+](\S+)', comment)
 
@@ -8,3 +9,4 @@ with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
         print(f'RUN_ID={match.group(1)}', file=f)
     else:
         print(f'VAL_FOUND=false', file=f)
+assert wandb.__version__ == '2.1.01', f'Expected version 2.1.01, but got {wandb.__version__}'
